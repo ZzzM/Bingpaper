32aa8d893b0b2008126cc3d04606e4977d9ddbbf
zm <zhangmeng067@163.com>
2022-04-24 17:24:35 +0800

    Updata scripts

--- scripts/generate_changlog.sh
+++ scripts/generate_changlog.sh
@@ -1,18 +1,3 @@
-
-# function read_mobileprovision() {
-#     local certificate_path=$RUNNER_TEMP/$APP_NAME.p12
-#     local pp_path=$RUNNER_TEMP/$APP_NAME.mobileprovision
-#     local pp_ne_path=$RUNNER_TEMP/${APP_NAME}_NE.mobileprovision
-
-#     echo -n $CERTIFICATE_DATA | base64 --decode --output $certificate_path
-#     echo -n $ALPHA_PROFILE_DATA | base64 --decode --output $pp_path
-#     echo -n $ALPHA_NE_PROFILE_DATA | base64 --decode --output $pp_ne_path
-
-#     echo "CERTIFICATE_PATH=$certificate_path" >> $GITHUB_ENV
-#     echo "PP_PATH=$pp_path" >> $GITHUB_ENV
-#     echo "PP_NE_PATH=$pp_ne_path" >> $GITHUB_ENV
-# }
-
 function generate_changlog() {
     local changelog=`cat changelogs/CHANGELOG_SC.md`
     changelog=${changelog%%---*}
@@ -21,5 +6,14 @@ function generate_changlog() {
     echo "$changelog" > $FIR_APP_CHANGELOG
 }
 
+function clean() {
+    rm -vfr ~/Library/Developer/Xcode/Archives/*
+}
+
+function upload() {
+    fastlane fir_cli
+}
 
 generate_changlog
+upload
+clean
